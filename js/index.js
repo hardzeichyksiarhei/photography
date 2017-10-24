2000232849f69239639eacded61a873b57ee4f81
hardz <hardzeichyksiarhei@gmail.com>
2017-10-24 22:55:36 +0300

    lang ru

--- js/index.js
+++ js/index.js
@@ -1,1 +1,1 @@
-webpackJsonp([2],{8:function(i,n,o){"use strict";(function(i){o(1),o(9),o(2),i(function(){i("#portfolio-grid").justifiedGallery({rowHeight:320,margins:20})})}).call(n,o(0))},9:function(i,n){}},[8]);
+webpackJsonp([2],{10:function(i,n){},9:function(i,n,o){"use strict";(function(i){o(1),o(10),o(2),i(function(){i("#portfolio-grid").justifiedGallery({rowHeight:320,margins:20})})}).call(n,o(0))}},[9]);
--- js/portfolio.js
+++ js/portfolio.js
@@ -1,1 +1,1 @@
-webpackJsonp([1],{10:function(i,t,n){"use strict";(function(i){n(1),n(11),n(2),i(function(){i("#portfolio-grid").justifiedGallery({rowHeight:200,margins:20,lastRow:"justify"})})}).call(t,n(0))},11:function(i,t){}},[10]);
+webpackJsonp([1],{11:function(i,t,n){"use strict";(function(i){n(1),n(12),n(2),i(function(){i("#portfolio-grid").justifiedGallery({rowHeight:200,margins:20,lastRow:"justify"})})}).call(t,n(0))},12:function(i,t){}},[11]);
