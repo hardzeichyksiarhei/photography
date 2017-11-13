404e238c8807e0c48c9d67577b9db2ce02122119
hardz <hardzeichyksiarhei@gmail.com>
2017-11-13 22:06:02 +0300

    fix

---
+++ js/index.js
@@ -0,0 +1,39 @@
+webpackJsonp([2],{
+
+/***/ 10:
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ 9:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function($) {
+
+__webpack_require__(1);
+
+__webpack_require__(10);
+
+__webpack_require__(2);
+
+$(function () {
+
+    //---------------- Plugins -----------------
+    //------------------------------------------
+
+    $('#portfolio-grid').justifiedGallery({
+        rowHeight: 320,
+        margins: 20
+    });
+
+    //------------------------------------------
+    //------------------------------------------
+});
+/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
+
+/***/ })
+
+},[9]);
